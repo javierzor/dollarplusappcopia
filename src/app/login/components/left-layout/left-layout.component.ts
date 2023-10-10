d85bd6221b8051173e33cb902c8a3ec0bc9198f6
Patrick Tanta <43278282+PatrickTanta@users.noreply.github.com>
2023-10-09 23:34:15 -0500

    feat: overriding css for login auth in responsive

---
+++ src/app/login/components/left-layout/left-layout.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-left-layout',
+    templateUrl: './left-layout.component.html',
+    styleUrls: ['./left-layout.scss']
+})
+export class LeftLayoutComponent {}
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { LoginPageRoutingModule } from './login-routing.module';
 
 import { LoginPage } from './login.page';
+import { LeftLayoutComponent } from './components/left-layout/left-layout.component';
 
 @NgModule({
   imports: [
@@ -17,6 +18,6 @@ import { LoginPage } from './login.page';
     IonicModule,
     LoginPageRoutingModule
   ],
-  declarations: [LoginPage]
+  declarations: [LoginPage, LeftLayoutComponent]
 })
 export class LoginPageModule {}
