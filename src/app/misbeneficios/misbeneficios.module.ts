092f84c16ca5dd7f0df2163cd32aabdd64425551
Patrick Tanta <43278282+PatrickTanta@users.noreply.github.com>
2023-10-10 11:03:29 -0500

    feat: add css for my benefits and help page

--- src/app/misbeneficios/misbeneficios.module.ts
+++ src/app/misbeneficios/misbeneficios.module.ts
@@ -1,20 +1,45 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { FormsModule } from '@angular/forms'
 
-import { IonicModule } from '@ionic/angular';
+import { IonicModule } from '@ionic/angular'
 
-import { MisbeneficiosPageRoutingModule } from './misbeneficios-routing.module';
+import { MisbeneficiosPageRoutingModule } from './misbeneficios-routing.module'
 
-import { MisbeneficiosPage } from './misbeneficios.page';
+import { MisbeneficiosPage } from './misbeneficios.page'
+import * as CryptoJS from 'crypto-js'
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    MisbeneficiosPageRoutingModule
+    MisbeneficiosPageRoutingModule,
   ],
-  declarations: [MisbeneficiosPage]
+  declarations: [MisbeneficiosPage],
 })
-export class MisbeneficiosPageModule {}
+export class MisbeneficiosPageModule {
+  secretKey = '123456&Descryption'
+  profileInfo: any = null
+
+  constructor() {
+    this.initializeApp()
+  }
+
+  async initializeApp() {
+    this.profileInfo = localStorage.getItem('profileInfo')
+    if (this.profileInfo) {
+      // se Cumplio: ProfileInfo existe en el cache
+      this.profileInfo = await this.decrypt(this.profileInfo)
+    }
+  }
+
+  async decrypt(textToDecrypt: string): Promise<string> {
+    if (textToDecrypt) {
+      return await CryptoJS.AES.decrypt(
+        textToDecrypt,
+        this.secretKey.trim()
+      ).toString(CryptoJS.enc.Utf8)
+    }
+  }
+}
